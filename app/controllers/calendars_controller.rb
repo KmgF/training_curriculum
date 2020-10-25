15854e56541f77a80ae668f9ce1d0de7cd9685e8
KmgF <72263892+KmgF@users.noreply.github.com>
2020-10-25 14:32:01 +0900

    resotre押しちゃった

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,12 +34,16 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< Updated upstream
       wday_num = (@todays_date + x).wday  #Date.today.wdayを利用して添字となる数値を得る
       #もしもwday_numが7以上であれば、7を引く
       if wday_num >= 7
         wday_num = wday_num -7
       end
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, week_days: wdays[wday_num] ,:plans => today_plans}
+=======
+      days = { :month :(@todays_date + x).month, :date :(@todays_date+x).day, :plans :today_plans}
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
 
